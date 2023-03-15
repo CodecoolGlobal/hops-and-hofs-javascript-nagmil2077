380008a31db3ad277f1587fef3bc0526703c5e09
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2023-03-15 15:38:30 +0100

    Finish task 2

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,8 @@
-const buttonComponent = (id, text) => `<button id="${id}">${text}</button>`;
+const buttonComponent = (id, text) => `<button id=${id}>${text}</button>`;
 
-const beerTypeComponent = list => list.map(tag => `<li>${tag}</li>`);
+const beerTypeComponent = (list) => list.map((tag) => `<li>${tag}</li>`).join('');
 
-const beerComponent = ({brewery, name, type, score, abv}) => `
+const beerComponent = ({ brewery, name, type, score, abv }) => `
 	<div class="beer">
 		<h2>${name}</h2>
 		<h3>${brewery}</h3>
@@ -16,23 +16,27 @@ const winnerComponent = (beer) => `
 	<div id="winner">
 		<h1>The best light Ale is</h1>
 		${beerComponent(beer)}
-		${buttonComponent("closeWinner", "Close")}
+		${buttonComponent('closeWinner', 'Close')}
 	</div>
 `;
 
-const loadEvent = _ => {
-	// the HTML elements with ID are available as global variables with the ID (eg. root) but it is better if you 
-	const rootElement = document.getElementById("root");
+const loadEvent = (_) => {
+  // the HTML elements with ID are available as global variables with the ID (eg. root) but it is better if you 
+  const rootElement = document.getElementById('root');
 
-	//You can add the HTML code to the DOM like this
-	rootElement.insertAdjacentHTML("afterbegin", buttonComponent());
+  //You can add the HTML code to the DOM like this
+  rootElement.insertAdjacentHTML('afterbegin', buttonComponent('loadBeers', 'Load the beers'));
 
-	const clickEvent = event => {
-		console.dir(event.target);
-		console.dir(event.target.id);
-	}
-	window.addEventListener("click", clickEvent);
-}
+  const clickEvent = (event) => {
+    console.dir(event.target);
+    console.dir(event.target.id);
+    if (event.target.id === 'loadBeers') {
+      document.getElementById('loadBeers').remove();
+      beers.map((beer) => rootElement.insertAdjacentHTML('afterbegin', `<section>${beerComponent(beer)}</section>`));
+    }
+  };
+  window.addEventListener('click', clickEvent);
+};
 
 // you can run your code in different ways but this is the safest. This way you can make sure that all the content (including css, fonts) is loaded.
-window.addEventListener("load", loadEvent);
+window.addEventListener('load', loadEvent);
