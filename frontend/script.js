e5af6a68c5360bbf4a70b5fb134aad11310eceda
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2023-03-16 10:24:26 +0100

    Modify task 1-4 solution

--- frontend/script.js
+++ frontend/script.js
@@ -24,29 +24,33 @@ const sortButton = (rootLocation) => {
   rootLocation.insertAdjacentHTML('afterbegin', buttonComponent('sortByScore', 'Sort by score'));
 };
 
-const loadEvent = (_) => {
+const loadEvent = () => {
   let clickCount = 0;
   // the HTML elements with ID are available as global variables with the ID (eg. root) but it is better if you
   const rootElement = document.getElementById('root');
-
   //You can add the HTML code to the DOM like this
   rootElement.insertAdjacentHTML('afterbegin', buttonComponent('loadBeers', 'Load the beers'));
+
+  rootElement.insertAdjacentHTML('beforeend', '<section id=\'beerSect\'></section>');
+  const sectionElement = document.getElementById('beerSect');
 
   const clickEvent = (event) => {
     console.dir(event.target);
     console.dir(event.target.id);
     if (event.target.id === 'loadBeers') {
       document.getElementById('loadBeers').remove();
-      beers.map((beer) => rootElement.insertAdjacentHTML('beforeend', `<section>${beerComponent(beer)}</section>`));
+      beers.map((beer) => sectionElement.insertAdjacentHTML('beforeend', beerComponent(beer)));
       sortButton(rootElement);
     } else if (event.target.id === 'sortByScore') {
-      document.querySelectorAll('section').forEach((beer) => beer.remove());
+      [...document.querySelectorAll('.beer')].map((beer) => beer.remove());
       const sortedBeerList = [...beers].sort((a, b) => a.score - b.score);
       if (clickCount % 2 === 0) {
-        sortedBeerList.map((beer) => rootElement.insertAdjacentHTML('beforeend', `<section>${beerComponent(beer)}</section>`));
+        sortedBeerList.map((beer) =>
+          sectionElement.insertAdjacentHTML('beforeend', beerComponent(beer)));
         clickCount++;
       } else {
-        sortedBeerList.reverse().map((beer) => rootElement.insertAdjacentHTML('beforeend', `<section>${beerComponent(beer)}</section>`));
+        sortedBeerList.reverse().map((beer) =>
+          sectionElement.insertAdjacentHTML('beforeend', beerComponent(beer)));
         clickCount++;
       }
     }
